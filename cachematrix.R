0f989bc37446c21db1362d151e6b745bb59bf1d9
daniel antohe <dantohe@gmail.com>
2015-04-20 15:43:40 -0600

    solving assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        #the following will store the reversed matrix
+        matrixInverse<-NULL
+        
+        #a setter
+        set<-function(y){
+                x<<-y
+                matrixInverse<<-NULL
+        }
+        
+        #a getter
+        get<-function() x
+        
+        #a setter for the inverse
+        setInverse<-function(solve) matrixInverse <<- solve
+        
+        #a getter for the inverse
+        getInverse<-function() matrixInverse
+        
+        #putting everything is a list
+        list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
@@ -12,4 +31,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        matrixInverse<-x$getInverse()
+        
+        #if no inverse created then print the message and retunr the inverse 
+        if(!is.null(matrixInverse)){
+                message("getting cached reversed matrix")
+                return(matrixInverse)
+        }
+        
+        #if no inverse has been yet generated then generate one
+        data<-x$get()
+        matrixInverse<-solve(data, ...)
+        
+        #store the just calculated inverse
+        x$setInverse(matrixInverse)
+        
+        #return the inverse
+        matrixInverse
 }
+
+
